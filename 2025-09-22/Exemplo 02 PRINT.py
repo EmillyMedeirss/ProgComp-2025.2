9b6b4772ac9760ba77a8e382c7d698f722c3e483
EmillyMedeirss <emillygabriele.llh77@gmail.com>
2025-09-22 20:29:06 -0300

    Primeira Aula Programação

---
+++ 2025-09-22/Exemplo 02 PRINT.py
@@ -0,0 +1,12 @@
+from email.policy import EmailPolicy
+
+
+print("Olá, mundo!")
+nome = 'Emilly'
+
+idade = 22
+
+print ('Olá', nome,', você tem', idade, 'anos.')
+
+print(f'Olá {nome}, você tem {idade} anos.')
+
