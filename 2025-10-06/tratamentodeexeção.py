035c8db4215ce5ecae0a1cfbff3812c6159b3c82
EmillyMedeirss <emillygabriele.llh77@gmail.com>
2025-10-06 20:40:45 -0300

    Erros e Exceções

--- 2025-10-06/tratamentodeexeção.py
+++ 2025-10-06/tratamentodeexeção.py
@@ -9,10 +9,10 @@ import sys
 try:
    raio = float(input('Informe o raio: '))
 except Exception as strErro:
-   #sys.exit(f'EXCEÇÃO: {sys.exc_info()[0]} -> {strErro}')
+   sys.exit(f'EXCEÇÃO: {sys.exc_info()[0]} -> {strErro}')
 
- if raio <= 0:
-      sys.exit('ERRO: O Raio deve ser positivo...')
+if raio <= 0:
+   sys.exit('ERRO: O Raio deve ser positivo...')
 area = 3.1416 * raio ** 2
 
 print(f'A área do círculo de raio = {raio} é {area:.2f}.')
