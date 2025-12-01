782530350debd971b9be024a195d085934ac3681
EmillyMedeirss <wmillygabriele.llh77@gmail.com>
2025-12-01 08:55:29 -0300

    atua

--- A2025-09-24/ATVD 3.py
+++
@@ -1,8 +0,0 @@
-#Solicite ao usuário uma temperatura em graus Celsius.
-#Converta para Fahrenheit.
-
-celsius = float(input('Informe a temperatura atual em graus Celsius'))
-
-fahrenheit = celsius * 9 /5 + 32
-
-print(f'A temperatura atual está por volta de {celsius:.1f}°C e {fahrenheit}°F.')
--- A2025-09-24/ATVD 4.py
+++
@@ -1,9 +0,0 @@
-#Peça ao usuário a distância percorrida (em km) e o tempo gasto (em horas).
-#Calcule a velocidade média e exiba o resultado.
-
-distancia  = float(input('Qual a distância percorrida em KM?'))
-horas      = float(input('Quanto tempo foi gasto em horas?'))
-
-velocidade = distancia / horas 
-
-print(f'A velocidade média foi de {velocidade:.2f}km/h.')
